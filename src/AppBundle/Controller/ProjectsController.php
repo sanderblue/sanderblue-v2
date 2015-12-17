f5d307d81b1eb025490ced416eee36f6e9a14794
Sander Blue <william.a.blue@gmail.com>
2015-12-17 10:09:31 -0600

    updating the projects page

--- src/AppBundle/Controller/ProjectsController.php
+++ src/AppBundle/Controller/ProjectsController.php
@@ -13,7 +13,7 @@ class ProjectsController extends Controller
         return $this->render('projects/index.html.twig', [
             'projects' => [
                 [
-                    'name' => 'The Solar System in 3D',
+                    'title' => 'The Solar System in 3D',
                     'description' => "A scaled 3D model of the Solar System, powered by Three.js and WebGL. All objects within this project have been modeled to scale based on real astronomical data. This includes the planets' rotations, orbits, and geometry. The scene currently renders the sun, thousands of stars, the asteriod belt, the planets and their moons.",
                     'github' => 'https://github.com/sanderblue/solar-system-threejs',
                     'bower' => null,
@@ -21,7 +21,7 @@ class ProjectsController extends Controller
                     'thumbnail' => 'assets/img/projects/thumbnail-solar-system-threejs.jpg',
                 ],
                 [
-                    'name' => 'Data Visualizations',
+                    'title' => 'Data Visualizations',
                     'description' => 'A project to explore data visualization utilizing the Chart.js library.',
                     'github' => 'https://github.com/sanderblue/data-visualizations',
                     'bower' => null,
@@ -29,7 +29,7 @@ class ProjectsController extends Controller
                     'thumbnail' => 'assets/img/projects/thumbnail-data-visualizations.jpg',
                 ],
                 [
-                    'name' => 'GitGraph',
+                    'title' => 'GitGraph',
                     'description' => 'Displays the previous 7 days of commit data on a Morris.js line graph.',
                     'github' => 'https://github.com/sanderblue/gitGraph',
                     'bower' => null,
@@ -37,7 +37,7 @@ class ProjectsController extends Controller
                     'thumbnail' => 'assets/img/projects/thumbnail-gitgraph.jpg',
                 ],
                 [
-                    'name' => 'AjaxRequest',
+                    'title' => 'AjaxRequest',
                     'description' => 'Minimalist AJAX request module with zero dependencies.',
                     'github' => 'https://github.com/sanderblue/ajaxrequest',
                     'bower' => null,
